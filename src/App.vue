9452a6937346baee35f919e0a1c160ee93f16bc8
avarlamova <59831804+avarlamova@users.noreply.github.com>
2023-04-29 18:16:24 +0300

    add tabs to view components

--- src/App.vue
+++ src/App.vue
@@ -2,11 +2,30 @@
   <!-- <LinkField />
   <DateTime />
   <MyMultiSelect />
-  <MultilineText /> -->
-  <SearchTable />
+  <MultilineText /> 
+  <SearchTable /> -->
+  <div class="card">
+    <TabView @tab-change="changeTab" activeIndex="active">
+      <TabPanel v-for="tab in tabs" :key="tab.id" :header="tab.label">
+        <p>{{ tab.content }}</p>
+      </TabPanel>
+    </TabView>
+    <component :is="currentTab"></component>
+  </div>
+
+  <!--  <TabMenu
+      v-model:activeIndex="active"
+      :model="tabs"
+      @tab-change="changeTab(e)"
+    /> -->
 </template>
 
 <script>
+import TabMenu from "primevue/tabmenu";
+
+import TabView from "primevue/tabview";
+import TabPanel from "primevue/tabpanel";
+
 import LinkField from "./components/LinkField.vue";
 import DateTime from "./components/DateTime.vue";
 import MyMultiSelect from "./components/MyMultiSelect.vue";
@@ -16,11 +35,53 @@ import SearchTable from "./components/SearchTable.vue";
 export default {
   name: "App",
   components: {
+    TabMenu,
+    TabView,
+    TabPanel,
     LinkField,
     DateTime,
     MyMultiSelect,
     MultilineText,
     SearchTable,
+  },
+  data() {
+    return {
+      active: 1,
+      currentTab: "LinkField",
+      tabs: [
+        { id: 1, label: "LinkField", icon: "pi pi-fw pi-home" },
+        {
+          id: 2,
+          label: "DateTime",
+          icon: "pi pi-fw pi-calendar",
+          componentName: "DateTime",
+        },
+        {
+          id: 3,
+          label: "MultiSelect",
+          icon: "pi pi-fw pi-pencil",
+          componentName: "MyMultiSelect",
+        },
+        {
+          id: 4,
+          label: "MultilineText",
+          icon: "pi pi-fw pi-file",
+          componentName: "MultilineText",
+        },
+        {
+          id: 5,
+          label: "SearchTable*",
+          icon: "pi pi-fw pi-cog",
+          componentName: "SearchTable",
+        },
+      ],
+    };
+  },
+  methods: {
+    changeTab(event) {
+      const { index } = event;
+      this.currentTab = this.tabs[index].componentName;
+    },
   },
 };
 </script>
