4e0148e5cdb8f4756f67853a9b378ac2d949f477
avarlamova <59831804+avarlamova@users.noreply.github.com>
2023-04-30 21:19:38 +0300

    update to script setup

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,17 @@
 <template>
-  <!-- <LinkField />
-  <DateTime />
-  <MyMultiSelect />
-  <MultilineText /> 
-  <SearchTable /> -->
   <div class="card">
-    <TabView @tab-change="changeTab" activeIndex="active">
+    <TabView @tab-change="changeTab">
       <TabPanel v-for="tab in tabs" :key="tab.id" :header="tab.label">
         <p>{{ tab.content }}</p>
       </TabPanel>
     </TabView>
-    <component :is="currentTab"></component>
+    <KeepAlive>
+      <component :is="currentComponent"></component>
+    </KeepAlive>
   </div>
-
-  <!--  <TabMenu
-      v-model:activeIndex="active"
-      :model="tabs"
-      @tab-change="changeTab(e)"
-    /> -->
 </template>
 
-<script>
-import TabMenu from "primevue/tabmenu";
-
+<script setup>
 import TabView from "primevue/tabview";
 import TabPanel from "primevue/tabpanel";
 
@@ -32,57 +21,45 @@ import MyMultiSelect from "./components/MyMultiSelect.vue";
 import MultilineText from "./components/MultilineText.vue";
 import SearchTable from "./components/SearchTable.vue";
 
-export default {
-  name: "App",
-  components: {
-    TabMenu,
-    TabView,
-    TabPanel,
-    LinkField,
-    DateTime,
-    MyMultiSelect,
-    MultilineText,
-    SearchTable,
+import { ref } from "vue";
+
+const tabs = [
+  {
+    id: 1,
+    label: "LinkField",
+    icon: "pi pi-fw pi-home",
+    component: LinkField,
   },
-  data() {
-    return {
-      active: 1,
-      currentTab: "LinkField",
-      tabs: [
-        { id: 1, label: "LinkField", icon: "pi pi-fw pi-home" },
-        {
-          id: 2,
-          label: "DateTime",
-          icon: "pi pi-fw pi-calendar",
-          componentName: "DateTime",
-        },
-        {
-          id: 3,
-          label: "MultiSelect",
-          icon: "pi pi-fw pi-pencil",
-          componentName: "MyMultiSelect",
-        },
-        {
-          id: 4,
-          label: "MultilineText",
-          icon: "pi pi-fw pi-file",
-          componentName: "MultilineText",
-        },
-        {
-          id: 5,
-          label: "SearchTable*",
-          icon: "pi pi-fw pi-cog",
-          componentName: "SearchTable",
-        },
-      ],
-    };
+  {
+    id: 2,
+    label: "DateTime",
+    icon: "pi pi-fw pi-calendar",
+    component: DateTime,
   },
-  methods: {
-    changeTab(event) {
-      const { index } = event;
-      this.currentTab = this.tabs[index].componentName;
-    },
+  {
+    id: 3,
+    label: "MultiSelect",
+    icon: "pi pi-fw pi-pencil",
+    component: MyMultiSelect,
   },
+  {
+    id: 4,
+    label: "MultilineText",
+    icon: "pi pi-fw pi-file",
+    component: MultilineText,
+  },
+  {
+    id: 5,
+    label: "SearchTable*",
+    icon: "pi pi-fw pi-cog",
+    component: SearchTable,
+  },
+];
+const currentComponent = ref(LinkField);
+
+const changeTab = (event) => {
+  const { index } = event;
+  currentComponent.value = tabs[index].component;
 };
 </script>
 
